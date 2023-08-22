16d74417fc72e7e8851206559dc115a31b3003bb
Universenn <dnwndhks98@gmail.com>
2023-08-22 15:43:10 +0900

    [build] first commit

---
+++ src/main/java/com/example/springbootguid/SpringBootGuidApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootguid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGuidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGuidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootguid/SpringBootGuidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootguid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGuidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
